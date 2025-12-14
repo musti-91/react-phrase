8783604261bf80bc1afe7240beea908482871bd7
Mustafa <mustafa.alroomi.91@gmail.com>
2025-12-14 12:53:14 +0300

    update flow

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,30 +1,30 @@
-import * as Polyglot from "node-polyglot";
+import * as Polyglot from "node-polyglot"
 
-import { Phrase } from "../helper";
+import { Phrase } from "../helper"
 
-const polyglot = new Polyglot({ phrases: {} });
+const polyglot = new Polyglot({ phrases: {} })
 
 export const getLocale = (() => {
 	if (navigator && navigator.languages && navigator.languages.length !== 0) {
-		return navigator.languages[0].substring(0, 2);
+		return navigator.languages[0].substring(0, 2)
 	}
-	return "en";
-})();
+	return "en"
+})()
 
 const getDictionary = (phrases: Phrase, lang: string) => {
 	if (Object.keys(phrases).length <= 1) {
-		throw new Error("No phrases have been defined, Or one language provided");
+		throw new Error("No phrases have been defined, Or one language provided")
 	}
 
-	return lang in phrases ? phrases[lang] : getLocale;
-};
+	return lang in phrases ? phrases[lang] : getLocale
+}
 
 /**
  * getTranslation function
  */
 export default (phrases: Phrase, lang: string) => {
-	polyglot.replace(getDictionary(phrases, lang));
-	polyglot.locale(lang);
+	polyglot.replace(getDictionary(phrases, lang))
+	polyglot.locale(lang)
 
-	return polyglot.t.bind(polyglot);
-};
+	return polyglot.t.bind(polyglot)
+}
